35b708c8950f78e316b6f2682be10449a75ee1c0
Hardik-1395 <hardik-13475@gmail.com>
2025-11-16 16:20:44 +0530

    cleaned the media summariser folder

--- backend/services/media_summariser/groqsummary.py
+++ backend/services/media_summariser/groqsummary.py
@@ -7,8 +7,12 @@ from IPython.display import  clear_output
 from dotenv import load_dotenv, find_dotenv
 import os
 import gc, torch, psutil, os
+from faster_whisper import WhisperModel, BatchedInferencePipeline
+from video_to_audio import convert_mp4_to_mp3  
+env_path = "/full/path/to/.env"
+load_dotenv(env_path)   
 
-# free Whisper model
+
 
 gc.collect()
 
@@ -16,7 +20,7 @@ if torch.cuda.is_available():
     torch.cuda.empty_cache()
 
 
-from faster_whisper import WhisperModel, BatchedInferencePipeline
+
 import torch
 device = 'cuda' if torch.cuda.is_available() else 'cpu' 
 load_dotenv()
@@ -26,7 +30,7 @@ if not HF_TOKEN:
 
 #video to audio part 
 
-from video_to_audio import convert_mp4_to_mp3  
+
 convert_mp4_to_mp3("elon.mp4", "sho.mp3")
 
 #audio to transcript 
--- backend/services/media_summariser/test1.py
+++
@@ -1,6 +0,0 @@
-import torch, subprocess
-print("torch:", torch.__version__)
-print("cuda available:", torch.cuda.is_available())
-print("cuda devices:", torch.cuda.device_count())
-# run once in terminal:
-# nvidia-smi
